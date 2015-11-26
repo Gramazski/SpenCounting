e55b939fe996ea256fa842a8de90379eaf98508b
Gramazski <gramazski@mail.ru>
2015-11-26 08:11:01 +0300

    Create SpenProject.dpr

---
+++ SpenProject.dpr
@@ -0,0 +1,13 @@
+program SpenProject;
+
+uses
+  Forms,
+  SpenUnit in 'SpenUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
